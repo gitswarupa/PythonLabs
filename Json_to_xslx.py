59f076294dd42a9bb031be3ec37286a3f7eba59b
Sannareddy, Krishnaswarupa <krishnaswarupa.sannareddy@experian.com>
2026-08-11 11:38:42 -0500

    Updated json data

--- Json_to_xslx.py
+++ Json_to_xslx.py
@@ -1,3 +1,4 @@
+#add Pandas library to convert nested JSON to excel file UV add pandas
 import pandas as pd
 data = [
     {
@@ -14,6 +15,20 @@ data = [
         "contact": {"email": "test2@example.com", "phone": "123-456-7890"},
         "location": {"city": "SPRING HILL", "state": "TN", "zip": "37174"},
     },
+      {
+            "id": 3,
+            "name": "test3",
+            "dob": "1990-01-03",
+            "contact": {"email": "test3@example.com", "phone": "123-456-7890"},
+            "location": {"city": "columbia", "state": "TN", "zip": "37046"},
+        },
+          {
+                "id": 4,
+                "name": "test3",
+                "dob": "1990-01-04",
+                "contact": {"email": "test4@example.com", "phone": "123-456-7890"},
+                "location": {"city": "brentwood", "state": "TN", "zip": "37027"},
+            },
 ]
 print(f"Nested JSON: \n{data}")
 df = pd.json_normalize(data)
